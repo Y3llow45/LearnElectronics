c3ce0e931b2dffb86038e92fc559feef06dae50e
Borislav Hristov <bofengejn@gmail.com>
2023-08-20 21:07:02 +0300

    Update Header, add css

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
 //import { Route, Switch, Redirect } from 'react-router-dom';
-import Header from './components/Header/Header'
-
+import Header from "./components/Header/Header"
+import { BrowserRouter } from 'react-router-dom';
 //<Route path="/" exact component={Lesons} />
 
 function App() {
   return (
-    <div className="container">
-      <Header />
-    </div>
+    <BrowserRouter>
+      <div className="container">
+        <Header />
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,17 +1,25 @@
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import './Header.css';
 
-const Header = () => {
+function Header() {
     return (
-        <header id="site-header">
-            <nav className="navbar">
-                <section className="navbar-dashboard">
-                    <div className="first-bar">
-                        <Link to="/">Dashboard</Link>
-                    </div>
-                </section>
-            </nav>
-        </header>
+        <nav className="navbar">
+            <section className="navbar-dashboard">
+                <div className="first-bar">
+                    <Link to="/" className="dashboard-link">Dashboard</Link>
+                    <Link to="/lessons" className="nav-link">Lessons</Link>
+                </div>
+            </section>
+            <section className="navbar-buttons">
+                <div className="second-bar">
+                    <Link to="/add-lessons" className="btn btn-primary">Add Lessons</Link>
+                    <Link to="/profile" className="btn btn-secondary">Profile</Link>
+                    <Link to="/login" className="btn btn-login">Log In</Link>
+                    <Link to="/signup" className="btn btn-signup">Sign Up</Link>
+                </div>
+            </section>
+        </nav>
     );
-};
+}
 
 export default Header;
