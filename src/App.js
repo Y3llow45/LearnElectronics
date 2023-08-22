069795a98e7c1c25478806db1cc474c8984028c0
Borislav Hristov <bofengejn@gmail.com>
2023-08-22 15:10:26 +0300

    Add routing and update Header

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import './App.css';
-//import { Route, Switch, Redirect } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import Header from "./components/Header/Header"
 import { BrowserRouter } from 'react-router-dom';
-//<Route path="/" exact component={Lesons} />
+import Home from './components/Home/Home';
+import Lessons from './components/Lessons/Lessons';
+import Add from './components/Add/Add';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="container">
         <Header />
+        <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/lessons" component={Lessons} />
+            <Route path="/add" component={Add} />
+          </Switch>
       </div>
     </BrowserRouter>
   );
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,6 @@
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import './Header.css';
+import userAvatar from '../../assets/userAvatar.png';
 
 //To Do: Hover efect on 'Electronics' text. Hover e => l,e,c glow in color: hsl(185, 62%, 45%);
 function Header() {
@@ -8,15 +9,16 @@ function Header() {
         <div className='nav-bar curved'>
             <h2 className='nav-h2-first'>Learn</h2><h2 className='nav-h2-second'>Electronics</h2> 
             <div className='nav-link-menu'>
-                <Link to='/' className='nav-link'>Home</Link>
-                <Link to='/lessons' className='nav-link'>Lessons</Link>
-                <Link to='/add' className='nav-link'>Add Lessons</Link>
-                <Link to='/signup' className='nav-link'>Sign up</Link>
+                <NavLink  to='/' className='nav-link'>Home</NavLink >
+                <NavLink  to='/lessons' className='nav-link'>Lessons</NavLink >
+                <NavLink  to='/add' className='nav-link'>Add lessons</NavLink >
+                <NavLink  to='/signup' className='nav-link'>Sign up</NavLink >
             </div>
             <div className='nav-user'>
-                <h3>Hello, anonymous!</h3>
+                <h3>Wellcome, Guest!</h3>
             </div>
-            <label htmlFor="menu-toggle" id="menu-icon">&#9776;</label>
+            <label htmlFor="menu-toggle" className="menu-icon" hidden>&#9776;</label>
+            <img className='nav-user-avatar' src={userAvatar} alt='UA' height='50px'/>
         </div>
         <svg viewBox="0 0 1440 320"><path fill="#fff" fillOpacity="1" d="M0,32L80,37.3C160,43,320,53,480,53.3C640,53,800,43,960,32C1120,21,1280,11,1360,5.3L1440,0L1440,0L1360,0C1280,0,1120,0,960,0C800,0,640,0,480,0C320,0,160,0,80,0L0,0Z"></path></svg>
         </div>
@@ -24,6 +26,11 @@ function Header() {
 }
 
 export default Header;
+
+/*{isAuthenticated
+                    ? <h3>Welcome, {username}!</h3>
+                    : <h3>Welcome, Guest</h3>
+                } */
 
 /*<button className="hide-nav-bar"></button>
             <input type="checkbox" id="menu-toggle"></input>
