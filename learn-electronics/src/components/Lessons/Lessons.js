f9fd71123317c874d3f3c9c6ce462360e37ebfa1
Borislav Hristov <bofengejn@gmail.com>
2023-08-23 13:20:31 +0300

    Update Lessons.js

--- learn-electronics/src/components/Lessons/Lessons.js
+++ learn-electronics/src/components/Lessons/Lessons.js
@@ -1,86 +1,55 @@
 import { Component } from "react";
 import './Lessons.css';
+import * as LessonService from '../../services/LessonServices';
 
 class Lessons extends Component {
-    componentDidMount() {
-        console.log('lessons');
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            lessons: [],
+        }
     }
-    render(){
+
+    componentDidMount() {
+        LessonService.getAll()
+            .then(res => {
+                console.log('Response from LessonService:', res);
+    
+                if (res && res.lessons) {
+                    this.setState({ lessons: res.lessons }, () => {
+                        console.log('New State is:', this.state.lessons);
+                    });
+                } else {
+                    console.error('Invalid data format:', res);
+                }
+            })
+            .catch(error => {
+                console.error('Error fetching lessons:', error);
+            });
+    }
+    render() {
+        const { lessons } = this.state;
+    
         return (
             <div className="lessons">
                 <h3>Lessons</h3>
-                <h3>Lessons</h3>
-                <h3>Lessons</h3>
-                <h3>Lessons</h3>
-                <h3>Lessons</h3>
-                <h3>Lessons</h3>
-                <h3>Lessons</h3>
-                <h3>Lessons</h3>
-                <h3>Lessons</h3>
-                <h3>Lessons</h3>
-                <h3>Lessons</h3>
-                <h3>Lessons</h3>
-                <h3>Lessons</h3>
-                <h3>Lessons</h3>
-                <h3>Lessons</h3>
-                <h3>Lessons</h3>
-                <h3>Lessons</h3>
-                <h3>Lessons</h3>
-                <h3>Lessons</h3>
-                <h3>Lessons</h3>
-                <h3>Lessons</h3>
-                <h3>Lessons</h3>
-                <h3>Lessons</h3>
-                <h3>Lessons</h3>
-                <h3>Lessons</h3>
-                <h3>Lessons</h3>
-                <h3>Lessons</h3>
-                <h3>Lessons</h3>
-                <h3>Lessons</h3>
-                <h3>Lessons</h3>
-                <h3>Lessons</h3>
-                <h3>Lessons</h3>
-                <h3>Lessons</h3>
-                <h3>Lessons</h3>
-                <h3>Lessons</h3>
-                <h3>Lessons</h3>
-                <h3>Lessons</h3>
-                <h3>Lessons</h3>
-                <h3>Lessons</h3>
-                <h3>Lessons</h3>
-                <h3>Lessons</h3>
-                <h3>Lessons</h3>
-                <h3>Lessons</h3>
-                <h3>Lessons</h3>
-                <h3>Lessons</h3>
-                <h3>Lessons</h3>
-                <h3>Lessons</h3>
-                <h3>Lessons</h3>
-                <h3>Lessons</h3>
-                <h3>Lessons</h3>
-                <h3>Lessons</h3>
-                <h3>Lessons</h3>
-                <h3>Lessons</h3>
-                <h3>Lessons</h3>
-                <h3>Lessons</h3>
-                <h3>Lessons</h3>
-                <h3>Lessons</h3>
-                <h3>Lessons</h3>
-                <h3>Lessons</h3>
-                <h3>Lessons</h3>
-                <h3>Lessons</h3>
-                <h3>Lessons</h3>
-                <h3>Lessons</h3>
-                <h3>Lessons</h3>
-                <h3>Lessons</h3>
-                <h3>Lessons</h3>
-                <h3>Lessons</h3>
-                <h3>Lessons</h3>
-                <h3>Lessons</h3>
-                <h3>Lessons</h3>
+                {lessons.map(lesson => (
+                    <div key={lesson.id}>
+                        <h3>{lesson.title}</h3>
+                        <p>{lesson.content}</p>
+                    </div>
+                ))}
             </div>
         );
     }
+    
 }
 
 export default Lessons;
+/*{lessons.map(lesson => (
+                    <li key={lesson.id}>
+                        <h3>{lesson.title}</h3>
+                        <p>{lesson.content}</p>
+                    </li>
+                ))}*/
