ad5cdf94311e1203ef229ef7cd384c444eef9edf
Borislav Hristov <bofengejn@gmail.com>
2023-08-24 10:52:37 +0300

    Update SearchBar.js to controlled class component

--- learn-electronics/src/components/Lessons/SearchBar/SearchBar.js
+++ learn-electronics/src/components/Lessons/SearchBar/SearchBar.js
@@ -1,28 +1,65 @@
-//import { Component } from 'react';
-import './SearchBar.css'
+import React, { Component } from 'react';
+import './SearchBar.css';
 
-function SearchBar() {
-    return (
-        <div className="search-bar">
-            <input
-                type="text"
-                placeholder="Search for lessons..."
-                value="{this.state.keyword}"
-                
-            />
-            <select value="{this.state.category}" >
-                <option value="All">All Categories</option>
-                <option value="Lessons">Lessons</option>
-                <option value="Electric Components">Electric Components</option>
-                <option value="Microcontrollers">Microcontrollers</option>
-            </select>
-            <select value="{this.state.likes}">
-                <option value="All">All Likes</option>
-                <option value="Liked">Liked</option>
-                <option value="Not Liked">Not Liked</option>
-            </select>
-            <button onClick="{this.handleSearch}">Search</button>
-        </div>
-    );
+class SearchBar extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            keyword: '',
+            category: 'All',
+            likes: 'All'
+        };
+    }
+
+    onInputChangeHandler = (event) => {
+        this.setState({ keyword: event.target.value }, () => {
+            console.log(this.state.keyword);
+        });
+    };
+
+    onCategoryChangeHandler = (event) => {
+        this.setState({ category: event.target.value }, () => {
+            console.log(this.state.category);
+        });
+    };
+
+    onLikesChangeHandler = (event) => {
+        this.setState({ likes: event.target.value }, () => {
+            console.log(this.state.likes);
+        });
+    };
+
+    handleSearch = (e) => {
+        e.preventDefault();
+        console.log(this.state);
+        // Call your search logic here using this.state values
+    };
+
+    render() {
+        return (
+            <div className="search-bar">
+                <input
+                    type="text"
+                    placeholder="Search for lessons..."
+                    value={this.state.keyword}
+                    onChange={this.onInputChangeHandler}
+                />
+                <select value={this.state.category} onChange={this.onCategoryChangeHandler}>
+                    <option value="All">All Categories</option>
+                    <option value="Lessons">Lessons</option>
+                    <option value="Electric Components">Electric Components</option>
+                    <option value="Microcontrollers">Microcontrollers</option>
+                </select>
+                <select value={this.state.likes} onChange={this.onLikesChangeHandler}>
+                    <option value="All">All Likes</option>
+                    <option value="Liked">Liked</option>
+                    <option value="Not Liked">Not Liked</option>
+                </select>
+                <button onClick={this.handleSearch.bind(this)}>Search</button>
+            </div>
+        );
+    }
 }
+
 export default SearchBar;
