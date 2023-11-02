d8c4f0e91cc9125f0270fb62db466be391f1d6e0
Borislav Hristov <bofengejn@gmail.com>
2023-11-02 18:44:54 +0200

    Update Home page

--- learn-electronics/src/components/Home/Home.js
+++ learn-electronics/src/components/Home/Home.js
@@ -4,22 +4,27 @@ import CanvasComponent from './Canvas/Canvas';
 function Home() {
   return (
       <div className="home-wlc-text-div">
-        <p className="home-wlc-text">At Learn Electronics, we're your gateway to the exciting world of electronics, microcontrollers, and more. Whether you're a beginner or a pro, our platform offers a treasure trove of knowledge and resources to help you explore and learn!</p>
-        
+        <p className="home-wlc-text big-text">At Learn Electronics, we're your gateway to the exciting world of electronics, microcontrollers, and more. Whether you're a beginner or a pro, our platform offers resources to help you explore and learn!</p>
+    
+        <div className='el-components-div'>
+          <p className='home-wlc-text el-components-text'>Electric components are the building blocks of electronics. They are the tiny parts that come together to create circuits and devices.</p>
+          <img src='../c.jpg'></img>
+        </div>
+
         <div className='canvas-div'>
           <CanvasComponent />
-          <p className="home-wlc-text">Microcontrollers are like tiny, smart computers that can make your home life easier. They can control lights, thermostats, and more.
+          <p className="microcontroller-text home-wlc-text">Microcontrollers are tiny computers. They can control lights, thermostats, motors and many more.
             In our "Microcontrollers" category, you'll discover how these little devices can automate everyday tasks.
-            Arduino, a popular microcontroller, is a great starting point for beginners. You can learn to build fun gadgets for your home.
-            Get started with microcontrollers, and you'll be amazed at how they can enhance your home environment.</p>
+            </p>
         </div>
         
         <div className='electronics-div'>
-          <p className="home-wlc-text">Electronics is all around us! It's the science behind phones, computers, and so much more.
-    If you're curious about how things work, electronics can be both a hobby and a career. It's a world of endless possibilities.
-    At our website, you can learn the basics of electronics, like circuits and components, in a beginner-friendly way.
-    Dive into electronics, and you'll have the skills to create your own gadgets and devices in no time.
-    Understanding electronics can open doors to endless innovations, from building robots to designing your own devices.</p>
+          <p className="home-wlc-text">Electronics are all around us! It's the science behind phones, computers, and so much more.
+            This can be both a hobby and a career. Understanding electronics can open doors to endless innovations, from building robots to designing your own devices.</p>
+            <div className='img-bottom-div'>
+            <img className='img-bottom' src='../1.png'></img>
+            <img className='img-bottom' src='../2.png'></img>
+            </div>
         </div>
       </div>
   );
