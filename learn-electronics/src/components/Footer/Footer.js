5e2ddfd6b924c7e97096db6727d9fc792b273d81
Borislav Hristov <bofengejn@gmail.com>
2023-11-02 20:44:36 +0200

    Update Footer

--- learn-electronics/src/components/Footer/Footer.js
+++ learn-electronics/src/components/Footer/Footer.js
@@ -7,6 +7,7 @@ function Footer() {
         <div className='my-footer'>
             <hr className="my-line"></hr>
             <p>Made with <span className='heart' role='img' aria-label='heart'>❤️</span> by <a className='username' href='https://github.com/Y3llow45'>Y3llow45</a></p>
+            <div className='big-div'>
             <div className='footer-div'>
                 <div className='tiny-div'>
                     <a className='footer-link' href='https://github.com/Y3llow45/LearnElectronics'>About this project</a>
@@ -18,19 +19,17 @@ function Footer() {
                     <a className='footer-link' href='/lessons/0'>Visit Lessons</a>
                     <a className='footer-link' href='/signup'>Visit Sign Up</a>
                 </div>
-                <button
-                    id="backToTopBtn"
-                    onClick={() => {
+            </div>
+            <div><img className='to-the-top' src='../arrow.png' onClick={() => {
                         console.log('clicked')
                         window.scrollTo({
                             top: 0, behavior: 'smooth'
                           });
-                    }}
-                >
-                  Back to Top
-                </button>
+                    }}></img>
+            </div>
             </div>
             <p>© 2023 LearnElectronics</p>
+            
         </div>
     );
 }
