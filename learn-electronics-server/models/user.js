c03505910f6baf6042d36d0fa2c1b104a886b2bc
Borislav Hristov <bofengejn@gmail.com>
2023-12-26 21:34:36 +0200

    Update user.js

--- learn-electronics-server/models/user.js
+++ learn-electronics-server/models/user.js
@@ -3,7 +3,9 @@ const mongoose = require("mongoose")
 const userSchema = new mongoose.Schema({
     username: String,
     email: String,
-    password: String
+    password: String,
+    role: 'user',
+    liked: []
 })
 
 module.exports = mongoose.model("User", userSchema)
